bd745add8f4dce29aea2dd6066e476cc9548f575
sharvs04 <142370907+sharvs04@users.noreply.github.com>
2023-10-12 14:34:19 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-hello world
+<?php
+
+include __DIR__ . "/../src/App/functions.php";
+
+$app = include __DIR__ . "/../src/App/boostrap.php";
+
+$app->run();
---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
+class HomeController {
+    public function home()
+    {
+        echo 'home page';
+    }
+
+}
